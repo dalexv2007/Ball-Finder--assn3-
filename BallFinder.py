ec5851941421761319467e0cc7f457555a048651
Daniel Villarreal <dalexv2007@outlook.com>
2026-04-22 16:37:36 -0500

    first commit

---
+++ BallFinder.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+from email.mime import image
+
+import rclpy
+import cv2
+import numpy as np
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, LaserScan
+from assn3.msg import BallLocation
+
+
+class Robot(Node):
+    def __init__(self):
+        super().__init__('ballfinder')
+        self.bridge = CvBridge()
+        self.raw_image = []
+        self.ranges = []
+
+        self.loc_publisher = self.create_publisher(BallLocation, '/ball_location', 10) #(type, topic, queue)
+        self.im_publisher = self.create_publisher(Image, '/ball_image', 10) # publisher for modified image
+        self.timer = self.create_timer(0.1, self.main_loop)
+        self.create_subscription( #get image data from robot's camera
+            Image,
+            '/oakd/rgb/preview/image_raw',
+            self.handle_image,
+            qos_profile_sensor_data,
+        )
+        self.create_subscription( #get scan data from robot's lidar
+            LaserScan,
+            '/scan',
+            self.handle_scan,
+            qos_profile_sensor_data,
+        )
+
+    def handle_image(self, msg):
+        try:
+            self.raw_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8') #convert ROS image to OpenCV format
+        except CvBridgeError:
+            print("Unable to convert ROS image to OpenCV format.") #error handler
+
+    def handle_scan(self, msg): 
+        self.ranges = msg.ranges # store scan data in self.ranges for use in main loop
+
+    def main_loop(self):
+        if len(self.raw_image) == 0 or len(self.ranges) == 0: #if nothing received, dont run.
+            return
+
+        image = self.raw_image.copy() #get image, use a copy to not modify original
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #convert image to hsv for filtering
+        lower_yellow = np.array([20, 100, 100]) #set bounds for yellow color in hsv
+        upper_yellow = np.array([30, 255, 255])
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow) #mask (binaryImage, lowBound, upBound), returns binary image where pixels in range are 255 and others are 0
+
+
+        yellow_cols = np.where(mask == 255)[1] #get column indices of yellow pixels,
+        ball_location = BallLocation() #ball_location = custom message of type BallLocation
+
+        if len(yellow_cols) == 0: #if no pixels found, set ball location to invalid values
+            ball_location.bearing = -1
+            ball_location.distance = -1.0        
+        else: #if pixels found...
+            avg_x = int(np.mean(yellow_cols)) #calculate average column index of yellow pixels... this is the "center" of the ball in the image
+            ball_location.bearing = avg_x #convert to bearing
+            scan_index = 335 - int(avg_x * 115 / 250) #convert avg_x to corresponding index in scan data (assuming 250 pixel width corresponds to 115 degree FOV of lidar, and lidar data is indexed from right to left)
+            distance = self.ranges[scan_index] #store distance for validity check
+            if np.isnan(distance) or np.isinf(distance): #ensure distance is valid number, if not set to -1.0 to indicate invalid distance
+                ball_location.distance = -1.0
+            else:
+                ball_location.distance = distance #if good, store distance in message
+            cv2.line(image, (avg_x, 0), (avg_x, image.shape[0]), (255, 0, 0), 2) #draw vertical blue line at avg_x to show ball center
+
+        image[mask>0] = (0, 255, 0) #set yellow pixels in original image to bright yellow for visualization
+
+        try:
+            self.im_publisher.publish(self.bridge.cv2_to_imgmsg(image, 'bgr8')) #publish modified image to ROS topic
+        except CvBridgeError:
+            print('Unable to convert mask to ROS image')
+        self.loc_publisher.publish(ball_location)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    robot = Robot()
+    rclpy.spin(robot)
+    robot.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
